e8ecaf1fe4e99d7abb91a274de8b97e9c5ea1cd3
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-16 22:59:14 -0700

    Update main.js
    Updated game logic with objects in array

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -11,10 +11,31 @@ console.log("User flipped " + cardFour);
 
 alert('Hello, Friends.')
 */ 
-//array for cards
-var cards=["queen","queen","king","king"];
+//array for cards setting up objects
+var cards=[
+	{
+	rank:"queen",
+	suit:"hearts",
+	cardImage:"images/queen-of-heats.png"
+	},
+	{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+	},
+	{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"image/king-hearts.png"
+	},
+	{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"image/king-of-diamonds.png"
+	}
+];
 var cardsInPlay=[];
-
+//check function if cards match
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
   console.log("You found a match!");
@@ -24,14 +45,23 @@ var checkForMatch = function(){
 	}
 }
 
-
+//flip card function with cardId parameters
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);	
+	//prints card flip in the cards array display rank object
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	//sends to check for match function by card size
 	if (cardsInPlay.length ===2){
 	checkForMatch();
+	
 	} 
 }
+
+
+
 flipCard(0);
 flipCard(2);
 
