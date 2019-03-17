73222ea10992ac4c879ccfb14a676fb3d77f431e
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-16 21:45:12 -0700

    Update main.js

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,8 +1,32 @@
 console.log("up and runing!")
-
+/*
 var cardOne="queen";
 var cardTwo="queen";
 var cardThree="king";
 var cardFour="king";
 
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
+
+alert('Hello, Friends.')
+*/ 
+
+var cards=["queen","queen","king","king"];
+var cardsInplay=[];
+var cardOne = cards[0];
+cardsInplay.push(cardOne);
+var cardTwo = cards[3];
+cardsInplay.push(cardTwo);
+
+if (cardsInplay.length == 2){
+	console.log(cardsInplay.length);
+	if (cardsInplay[0] == cardsInplay[1]){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.")
+	}
+}
+
+console.log("User flipped " + cardsInplay);
