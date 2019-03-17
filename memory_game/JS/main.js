79e990057a6f89dc64c270ebf469d1f1509307a5
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-16 22:38:16 -0700

    Update main.js

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -28,9 +28,9 @@ var checkForMatch = function(){
 var flipCard = function(cardId){
 	console.log("User flipped " + cards[cardId]);
 	cardsInPlay.push(cards[cardId]);	
-	
+	if (cardsInPlay.length ===2){
 	checkForMatch();
-	
+	} 
 }
 flipCard(0);
 flipCard(2);
